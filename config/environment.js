89380da350ccbdced979628ec04a1438a6e01967
Alex DiLiberto <alex@alexdiliberto.com>
2017-07-26 14:20:46 -0400

    ember-cli-deploy-git setup

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = '/freestyle-test-app';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
